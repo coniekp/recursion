507b4eec845e69140d676a6dbf1d4cb5d7fea004
Kony Pham <coniekp@icloud.com>
2018-05-28 13:24:00 -0500

    "Defined stringifyJSON"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,31 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+
+  //wrap strings
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+  //wrap arrays
+  if (Array.isArray(obj)) {
+    return '[' + obj.map (stringifyJSON) + ']';
+  }
+
+  //wrap Objects
+  if (typeof obj === 'object' && obj) {
+    var properties = [];
+    for (var key in obj) {
+      var hasValue = obj[key] !== undefined;
+      var isNotFunction = typeof obj[key] !== 'function';
+
+      if (hasValue && isNotFunction) {
+        properties.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+      }
+    }
+    return '{' + properties.join() + '}';
+  }
+
+  //wrap all other primitive valuess
+  return '' + obj;
 };
