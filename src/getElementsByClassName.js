3be07fd6869f311ecda53aaf836cd2ba19ba5bf9
Kony Pham <coniekp@icloud.com>
2018-05-28 15:08:57 -0500

    "Defined getElementsByClassName()"

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,28 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className, parent) {
   // your code here
+  var memo = [];
+
+  //recursive finder function that uses memoization
+  var innerFinder = function (node) {
+    node = node || document.body;
+
+    //base case
+    if (node.classList && node.classList.contains(className)) {
+      memo.push(node);
+    }
+
+    //find descendants with className
+    for (var i = 0; i < node.children.length; i++) {
+      innerFinder (node.children[i]);
+    }
+  };
+
+  //invocation of recursive finder
+  innerFinder(parent);
+
+  return memo;
+
 };
